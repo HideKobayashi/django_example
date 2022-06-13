f1ca61f7a051723dc8c71481eb2b94f7d9a3c6b9
Hideyuki KOBAYASHI <kobayashi.hideyuki@leadinge.co.jp>
2022-06-13 22:24:38 +0900

    bootstrap5 の導入

--- djangosnippets/djangosnippets/settings.py
+++ djangosnippets/djangosnippets/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'snippets.apps.SnippetsConfig',  # snippets アプリを追加
+    'django_bootstrap5',  # Bootstrap5
 ]
 
 MIDDLEWARE = [
