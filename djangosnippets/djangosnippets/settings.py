99866135e3d58b80e2f301968b6bfb9dff5a3a08
Hideyuki KOBAYASHI <kobayashi.hideyuki@leadinge.co.jp>
2022-06-19 15:15:07 +0900

    プロジェクトルート直下に base.html を移動

--- djangosnippets/djangosnippets/settings.py
+++ djangosnippets/djangosnippets/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'djangosnippets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # デフォルトは空リスト []
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
